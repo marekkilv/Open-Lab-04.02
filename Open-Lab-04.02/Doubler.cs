a4bbbe65569ba8469f5da00498b32ef848e00a8a
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 19:52:05 +0100

    mam

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string x = "ahoj";
+            foreach (char letter in original)
+                a += letter.ToString() + letter.ToString(); 
+            return x;        
         }
     }
 }
